60961a364d3b6d177a1de5e124d2ea6744366099
Roman Serediuk <box4rom@gmail.com>
2023-08-21 18:47:49 +0300

    feat: implement index, create, update, destroy acts

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::NewsController < ApplicationController
+  before_action :set_news, except: %i[index create]
+
+  def index
+    @news = News.all
+
+    render json: @news, status: 200
+  end
+
+  def create
+    @news = News.new news_params
+
+    if @news.save
+      render json: @news, status: :created
+    else
+      render json: @news.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @news.update news_params
+      render json: @news, status: :accepted
+    else
+      render json: @news.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @news.destroy
+
+    head :no_content
+  end
+  
+  private
+
+  def news_params
+    params.require(:news).permit(:title, :body, :date, links: [])
+  end
+
+  def set_news
+    @news = News.find params[:id]
+  end
+  
 end
