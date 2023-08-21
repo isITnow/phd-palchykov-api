017d01c8d082067e29b02fe949f062f16c48a3e9
Roman Serediuk <box4rom@gmail.com>
2023-08-21 15:52:27 +0300

    feat: add set_publication_period!; implement index, show actions

--- app/controllers/api/v1/publication_periods_controller.rb
+++ app/controllers/api/v1/publication_periods_controller.rb
@@ -1,7 +1,21 @@
 class Api::V1::PublicationPeriodsController < ApplicationController
+  before_action :set_publication_period!, only: %i[show]
   def index
+    @publication_periods = PublicationPeriod.all
+
+    render json: @publication_periods, status: 200
   end
 
   def show
+    @publications = @publication_period.publications
+
+    render json: @publications, status: 200
   end
+
+  private
+
+  def set_publication_period!
+    @publication_period = PublicationPeriod.find params[:id]
+  end
+  
 end
