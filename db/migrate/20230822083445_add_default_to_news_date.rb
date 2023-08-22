a87c46216e3b7dc77ffa8a55a38ac253459990bb
Roman Serediuk <box4rom@gmail.com>
2023-08-22 11:44:36 +0300

    fix(news): add default value to date column

---
+++ db/migrate/20230822083445_add_default_to_news_date.rb
@@ -0,0 +1,5 @@
+class AddDefaultToNewsDate < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :news, :date, from: nil, to: ""
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_151854) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_083445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_151854) do
   create_table "news", force: :cascade do |t|
     t.string "title", null: false
     t.text "body"
-    t.string "date"
+    t.string "date", default: ""
     t.string "links", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
