5e5318f9594a6a995d3346d2f7f50e5828d2db7a
Roman Serediuk <box4rom@gmail.com>
2023-08-25 15:52:12 +0300

    fix: include url_helpers

--- app/serializers/colleague_serializer.rb
+++ app/serializers/colleague_serializer.rb
@@ -1,4 +1,5 @@
 class ColleagueSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers  
   attributes :id, :name, :position, :phone, :email, :photo_url, :created_at
 
   def photo_url
