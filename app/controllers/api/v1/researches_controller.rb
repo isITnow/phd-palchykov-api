231ad6b3e25e7e635532927518f26d0304beb950
Roman Serediuk <box4rom@gmail.com>
2023-08-29 21:27:28 +0300

    feat: create researches_controller with index, create, destroy actions

---
+++ app/controllers/api/v1/researches_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::ResearchesController < ApplicationController
+  
+  def index
+    @researches = Research.includes(:illustrations)
+    render json: @researches, status: 200 
+  end
+
+  def create
+    @research = Research.new research_params
+
+    if @research.save
+      render json: @research, status: :created
+    else
+      render json: @research.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @@research.destroy
+
+    head :no_content
+  end
+  
+
+  private
+
+  def research_params
+    params.require(:research).permit(:payload)
+  end
+  
+end
