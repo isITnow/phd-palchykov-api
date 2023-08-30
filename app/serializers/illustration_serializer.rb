117695543701064d146bf2e5511c50464c937c9f
Roman Serediuk <box4rom@gmail.com>
2023-08-30 16:01:02 +0300

    create illustration_serializer

---
+++ app/serializers/illustration_serializer.rb
@@ -0,0 +1,12 @@
+class IllustrationSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers  
+  attributes :id, :description, :schema_url, :created_at
+
+  belongs_to :research
+
+  def schema_url
+    if object.schema.attached?
+      rails_blob_url(object.schema, only_path: true)
+    end
+  end
+end
