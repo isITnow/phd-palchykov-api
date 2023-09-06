f90f2bf8f3273c2e3947140b20e7069ddaafd3f6
Roman Serediuk <box4rom@gmail.com>
2023-09-06 13:54:52 +0300

    upd

--- app/serializers/news_serializer.rb
+++ app/serializers/news_serializer.rb
@@ -1,6 +1,6 @@
 class NewsSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers  
-  attributes :id, :title, :body, :links, :date, :image_url, :created_at
+  attributes :id, :title, :body, :image_url, :links, :date, :created_at
 
   def image_url
     if object.image.attached?
