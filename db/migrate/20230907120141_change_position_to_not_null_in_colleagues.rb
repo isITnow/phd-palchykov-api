48057895eec82e592fd32bd4eb1c6e1779ac85a0
Roman Serediuk <box4rom@gmail.com>
2023-09-07 15:07:03 +0300

    feat: create comments table; add "null: false" to research-payload and colleagues-position

---
+++ db/migrate/20230907120141_change_position_to_not_null_in_colleagues.rb
@@ -0,0 +1,5 @@
+class ChangePositionToNotNullInColleagues < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :colleagues, :position, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_084615) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_07_120408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,11 +44,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_084615) do
 
   create_table "colleagues", force: :cascade do |t|
     t.string "name", null: false
-    t.string "position"
+    t.string "position", null: false
     t.string "phone"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.string "author", default: "Guest User", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "devise_api_tokens", force: :cascade do |t|
@@ -111,7 +120,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_084615) do
   end
 
   create_table "researches", force: :cascade do |t|
-    t.jsonb "payload"
+    t.jsonb "payload", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -132,6 +141,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_084615) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
   add_foreign_key "illustrations", "researches"
   add_foreign_key "posts", "users"
   add_foreign_key "publications", "publication_periods"
