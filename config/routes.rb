130e528ce16d5a79dc87c442edd4702c5d60fbfc
Roman Serediuk <box4rom@gmail.com>
2023-09-07 11:59:23 +0300

    feat: create Post model/table/resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :users
+      
+      resources :colleagues, except: %i[new edit show]
+      resources :news, except: %i[new edit show]
+
+      resources :posts, except: %i[new edit]
 
       resources :publication_periods, only: %i[index create destroy] do
-        resources :publications, except: %i[show]
+        resources :publications, except: %i[new edit show]
       end
 
-      resources :colleagues, except: %i[show]
-      resources :news
       resources :researches, only: %i[index create destroy] do
         resources :illustrations, only: %i[create]
       end
