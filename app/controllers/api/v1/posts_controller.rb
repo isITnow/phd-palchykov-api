6a08b60d600d0677cd2b9c9e1bc0f8efb026439f
Roman Serediuk <box4rom@gmail.com>
2023-09-07 15:18:30 +0300

    feat: create posts_controller with index, create, destroy act

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token, raise: false  
+  before_action :authenticate_devise_api_token!, only: %i[:create :update :destroy]
+  before_action :set_post, except: %i[index create]
+
+  def index
+    @posts = Post.all.order(updated_at: :desc)
+
+    render json: @posts, status: 200
+  end
+
+  def create  
+    @post = current_user.posts.build posts_params
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update post_params
+      render json: @post, status: :accepted
+    else
+      render json: @post.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    head :no_content
+  end
+  
+  private
+
+  def posts_params
+    params.require(:post).permit(:body)
+  end
+
+  def set_post
+    @post = Post.find params[:id]
+  end
+
+end
