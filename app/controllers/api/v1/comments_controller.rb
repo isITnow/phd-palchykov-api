b5d64f0b0c2ac2ec6577bac6aa145fe395e703ae
Roman Serediuk <box4rom@gmail.com>
2023-09-11 21:47:19 +0300

    fix(create): add  skip_before_action :is_valid_token?, only: %i[create]

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false  
-  before_action :authenticate_devise_api_token!, only: %i[create destroy]
+  skip_before_action :is_valid_token?, only: %i[create]
+  before_action :authenticate_devise_api_token!, only: %i[destroy]
   before_action :set_post
   before_action :set_comment, only: %i[destroy]
 
