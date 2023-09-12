991b66e7e21d015cac6f35fccc994f3d0d247b48
Roman Serediuk <box4rom@gmail.com>
2023-09-12 14:36:39 +0300

    fix(module ErrorHandling): add neamespace

--- app/controllers/concerns/api/v1/error_handling.rb
+++ app/controllers/concerns/api/v1/error_handling.rb
@@ -1,13 +1,17 @@
-module ErrorHandling
-  extend ActiveSupport::Concern
-
-  included do
-    rescue_from ActiveRecord::RecordNotFound, with: :notfound
-
-    private
-
-    def notfound
-      render json: { error: 'Record not found' }, status: :not_found
+module Api
+  module V1
+    module ErrorHandling
+      extend ActiveSupport::Concern
+      
+      included do
+        rescue_from ActiveRecord::RecordNotFound, with: :notfound
+      end
+      
+      private
+      
+      def notfound
+        render json: { error: 'Record not found' }, status: :not_found
+      end
     end
   end
 end
