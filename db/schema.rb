8ca318993648e3aca765db55c639b773d5ff4e05
Roman Serediuk <box4rom@gmail.com>
2023-09-14 08:19:21 +0300

    feat: add title uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_120408) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_051551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,6 +105,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_120408) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_publication_periods_on_title", unique: true
   end
 
   create_table "publications", force: :cascade do |t|
