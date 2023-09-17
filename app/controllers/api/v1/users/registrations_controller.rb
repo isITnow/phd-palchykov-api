a016def947aef7ab8e5c629fcf0e95ff137cc80f
Roman Serediuk <box4rom@gmail.com>
2023-09-17 10:48:38 +0300

    feat: create signup logic

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,47 +1,20 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
   respond_to :json
 
   private
 
-  def respond_with(resource, _opts = {})
-    if request.method == "POST" && resource.persisted?
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
       render json: {
-        status: {code: 200, message: "Signed up sucessfully."},
-        data: resource
-      }, status: :ok
-    elsif request.method == "DELETE"
-      render json: {
-        status: { code: 200, message: "Account deleted successfully."}
-      }, status: :ok
+        status: {code: 200, message: 'Signed up successfully.'},
+        data: UserSerializer.new(current_user)
+      }
     else
       render json: {
-        status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
       }, status: :unprocessable_entity
     end
   end
-
-  protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username role])
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
