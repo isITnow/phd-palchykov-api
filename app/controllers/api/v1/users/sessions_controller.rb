f5b9264988cb893bb6b150c71890992d05d43a24
Roman Serediuk <box4rom@gmail.com>
2023-09-17 10:49:16 +0300

    feat: create login, logout logic

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
   respond_to :json
 
   private
 
-  def respond_with(resource, _opts = {})
+  def respond_with(current_user, _opts = {})
     render json: {
       status: {code: 200, message: 'Logged in sucessfully.'},
-      data: resource
+      data: UserSerializer.new(current_user)
     }, status: :ok
   end
 
   def respond_to_on_destroy
-    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
-    current_user = User.find(jwt_payload['sub'] )
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last,
+                    Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
     
     if current_user
       render json: {
@@ -30,10 +32,4 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
     end
   end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
