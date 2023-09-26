f910b6e439bff62e68e11da48f72dfff81ffcfad
Roman Serediuk <box4rom@gmail.com>
2023-09-26 20:30:36 +0300

    fix(photo_url): add only_path: true

--- app/serializers/colleague_serializer.rb
+++ app/serializers/colleague_serializer.rb
@@ -1,20 +1,10 @@
 class ColleagueSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers  
   attributes :id, :name, :position, :phone, :email, :photo_url, :created_at
-  
-  # def photo_url
-  #   if object.photo.attached?
-  #     url_for(object.photo)
-  #   end
-  # end
-  # def photo_url
-  #   if object.photo.attached?
-  #     rails_blob_url(object.photo, only_path: true)
-  #   end
-  # end
+
   def photo_url
     if object.photo.attached?
-      rails_blob_url(object.photo, default_url_options[:host])
+      rails_blob_url(object.photo, only_path: true)
     end
   end
 end
