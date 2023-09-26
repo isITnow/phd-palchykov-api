2fa5f3046ee1c631a015a0a1f1dbcba5a380b3ca
Roman Serediuk <box4rom@gmail.com>
2023-09-26 19:55:32 +0300

    upd: add config.routes.default_url_options[:host]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
+  # For generating absolute urls with routing url helpers.
+  config.routes.default_url_options[:host] = 'https://phd-palchykov-api.onrender.com'
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
