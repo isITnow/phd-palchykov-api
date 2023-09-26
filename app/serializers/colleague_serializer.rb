481d2c70a5a750494689b6532c35ac5b73f03384
Roman Serediuk <box4rom@gmail.com>
2023-09-26 20:05:36 +0300

    fix: override default_url_options in ApplicationController

--- app/serializers/colleague_serializer.rb
+++ app/serializers/colleague_serializer.rb
@@ -2,14 +2,14 @@ class ColleagueSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers  
   attributes :id, :name, :position, :phone, :email, :photo_url, :created_at
   
-  def photo_url
-    if object.photo.attached?
-      url_for(object.photo)
-    end
-  end
   # def photo_url
   #   if object.photo.attached?
-  #     rails_blob_url(object.photo, only_path: true)
+  #     url_for(object.photo)
   #   end
   # end
+  def photo_url
+    if object.photo.attached?
+      rails_blob_url(object.photo, only_path: true)
+    end
+  end
 end
