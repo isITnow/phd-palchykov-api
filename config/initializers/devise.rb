c6b7f257661d76ea0919681dae4b6d01ec66a3de
Roman Serediuk <box4rom@gmail.com>
2023-09-26 15:43:20 +0300

    fix(config.jwt): replace devise_jwt_secret_key! by devise_jwt_secret_key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,7 +313,7 @@ Devise.setup do |config|
 
   # Secret key configuration
 config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
