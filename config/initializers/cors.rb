f358df96c59170b2abd1d4ac31c9e08f9078ce51
Roman Serediuk <box4rom@gmail.com>
2023-10-03 15:23:23 +0300

    upd: origins "https://palchykovchem.vercel.app/", "http://127.0.0.1:3003"

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://palchykovchem.vercel.app/", "http://127.0.0.1:3003"
 
     resource "*",
       headers: :any,
