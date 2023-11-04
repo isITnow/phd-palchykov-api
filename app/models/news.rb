94790dbda755c235f98a954c7fb0f05b531d3f3c
Roman Serediuk <box4rom@gmail.com>
2023-11-04 18:05:47 +0200

    upd(validation): body; title length; date presence; add ensure_date_has_a_value method

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,18 @@
 class News < ApplicationRecord
-  validates :title, presence: true
+  before_validation :ensure_date_has_a_value
+
+  validates :body, length: { minimum: 10 }
+  validates :date, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 150 }
   
   has_one_attached :image
+
+  private
+
+  def ensure_date_has_a_value
+    if self.date.nil? || self.date.blank?
+      self.date = Time.zone.now.strftime('%B %e, %Y')
+    end
+  end
 end
 
