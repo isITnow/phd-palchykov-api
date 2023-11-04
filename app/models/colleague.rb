67c1ac3377a48c6cb776ca7b5bee23cefe4b47ee
Roman Serediuk <box4rom@gmail.com>
2023-11-04 20:48:30 +0200

    fix(photo validation): add presence: true

--- app/models/colleague.rb
+++ app/models/colleague.rb
@@ -2,6 +2,6 @@ class Colleague < ApplicationRecord
   has_one_attached :photo
   
   validates :name, :position, presence: true, length: { minimum: 5 }
-  validates :photo
+  validates :photo, presence: true
 end
 
