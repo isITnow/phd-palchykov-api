21481d5dd611f9146e9bcc739db92dcc8a7c8a9f
Roman Serediuk <box4rom@gmail.com>
2023-11-04 22:01:17 +0200

    feat: create Colleague factory, tests

---
+++ spec/factories/colleagues.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :colleague do
+    email { Faker::Internet.email}
+    name { Faker::Name.name }
+    phone { Faker::PhoneNumber.phone_number_with_country_code }
+    photo { 
+      {
+        io: File.open('spec/fixtures/default_image.jpg'),
+        filename: 'default_image.jpg',
+        content_type: 'image/jpeg'
+      }
+        }
+    position { Faker::Job.position }
+  end
+end
