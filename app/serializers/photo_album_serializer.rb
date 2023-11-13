787bbd81227736af190c14756fc27be6fab60d4f
Roman Serediuk <box4rom@gmail.com>
2023-11-13 13:35:16 +0200

    upd: add show_action? method; include :pictures_list attr if show action only

--- app/serializers/photo_album_serializer.rb
+++ app/serializers/photo_album_serializer.rb
@@ -1,7 +1,7 @@
 class PhotoAlbumSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers  
 
-  attributes :id, :title, :cover_image_url, :pictures_list, :created_at
+  attributes :id, :title, :cover_image_url, :created_at, :pictures_list
 
   def cover_image_url
     if object.cover_image.attached?
@@ -10,16 +10,22 @@ class PhotoAlbumSerializer < ActiveModel::Serializer
   end
   
   def pictures_list
-    if object.pictures
+    if object.pictures && show_action?
       object.pictures.map do |picture|
         {
           id: picture.id,
           filename: picture.filename,
-          url: url_for(picture),
-          metadata: picture.metadata
+          picture_url: url_for(picture),
+          metadata: {
+            width: picture.metadata[:width],
+            height: picture.metadata[:height]
+          }
         }
       end
     end
   end
-  
+
+  def show_action?
+    instance_options[:action_name] == "show"
+  end
 end
