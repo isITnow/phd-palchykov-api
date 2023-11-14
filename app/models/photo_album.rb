2840f4edb50f1f7acbc58135e86710a7ba31e594
Roman Serediuk <104834083+isITnow@users.noreply.github.com>
2023-11-14 21:02:40 +0200

    Update PhotoAlbum model validation (#16)
    * upd: remove "http://127.0.0.1:3003"
    
    * upd: add favicon
    
    * feat: inmplement research update; illustration update
    
    * fix: add belongs_to :user
    
    * update cors
    
    * feat: add user factory
    
    * add FactoryBot::Syntax::Methods
    
    * feat: add Post factory, tests
    
    * feat: add Comment factory, tests
    
    * upd: add length validation minimum: 5, maximum: 700
    
    * upd: add body length tests
    
    * upd(validation): body; title length; date presence; add ensure_date_has_a_value method
    
    * feat: add News factory, tests
    
    * upd(name,position validation): add length: { minimum: 5 }
    
    * fix(photo validation): add presence: true
    
    * feat: create Colleague factory, tests
    
    * upd(index, show): add action_name: action_name
    
    * upd: add show_action? method; include :pictures_list attr if show action only
    
    * upd(title): validate uniqueness

--- app/models/photo_album.rb
+++ app/models/photo_album.rb
@@ -2,5 +2,5 @@ class PhotoAlbum < ApplicationRecord
   has_one_attached :cover_image
   has_many_attached :pictures
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 end
