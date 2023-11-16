11df3df2c40cbeb793344681b813dc9c09f29a38
Roman Serediuk <box4rom@gmail.com>
2023-11-16 11:33:47 +0200

    feat: purge attachment

---
+++ app/controllers/api/v1/attachments_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::AttachmentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  include ErrorHandling
+
+  def purge
+    @attachment = ActiveStorage::Attachment.find params[:id]
+    @attachment.purge
+
+    head :no_content
+  end
+end
