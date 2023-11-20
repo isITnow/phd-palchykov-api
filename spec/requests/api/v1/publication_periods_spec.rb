7e572d131d7f1afec433761255f6446667f4de4a
Roman Serediuk <box4rom@gmail.com>
2023-11-20 18:18:09 +0200

    test: add PublicationPeriods requests tests

---
+++ spec/requests/api/v1/publication_periods_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+
+describe "Api::V1::PublicationPeriods", type: :request do
+  describe "GET /api/v1/publication_periods" do
+    it "returns a successful response" do
+      get api_v1_publication_periods_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns publication periods" do
+      periods = [
+        "2021-present",
+        "2016-2020",
+        "2011-2015","2005-2010",
+        "Books and chapters"
+      ]
+
+      periods.each do |period|
+          create(:publication_period, title: period)
+      end
+    
+    get api_v1_publication_periods_path
+    expect(JSON.parse(response.body).length).to eq(5)  
+    end
+  end
+
+  describe "POST /api/v1/publication_periods" do
+    let(:user) { create(:user) }
+    before { sign_in user }
+    
+    context "with valid params" do
+      let(:valid_attributes) { {title: "2020-2023"} }
+
+      it "returns a successful response" do
+        post api_v1_publication_periods_path params: { publication_period: valid_attributes }
+        expect(response).to have_http_status(:created)
+      end
+
+      it "creates a new PublicationPeriod" do
+        expect {
+          post api_v1_publication_periods_path params: { publication_period: valid_attributes }
+        }.to change(PublicationPeriod, :count).by(1)
+        publication_period = PublicationPeriod.last
+        expect(publication_period.title).to eq("2020-2023")
+      end
+    end
+
+    context "with invalid params" do
+      let(:invalid_attributes) { { string: "2020-2023" } }
+      context "with no user signed in" do
+        it "returns an unauthorized response" do
+          sign_out user
+          post api_v1_publication_periods_path params: { publication_period: { title: "2020-2023" } }
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+      
+      it "returns failure response" do
+        post api_v1_publication_periods_path params: { publication_period: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "does not create a new PublicationPeriod" do
+        expect {
+          post api_v1_publication_periods_path params: { publication_period: invalid_attributes }
+        }.to change(PublicationPeriod, :count).by(0)
+      end
+    end
+  end
+
+  describe "DELETE /api/v1/publication_periods/:id" do
+    let(:user) { create(:user) }
+    before { sign_in user }
+
+    let(:publication_period) {create(:publication_period)}
+    let(:publication_period_2) {create(:publication_period, title: "1986-1999")}
+
+    context "with no user signed in" do
+      it "returns an unauthorized response" do
+        sign_out user
+        delete api_v1_publication_period_path(publication_period)
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context "publication period exists" do
+      it "returns a successful response" do
+        delete api_v1_publication_period_path(publication_period)
+        expect(response).to have_http_status(:no_content)
+      end
+      
+      it "deletes a PublicationPeriod" do
+        period = publication_period
+        period_2 = publication_period_2
+
+        expect {
+          delete api_v1_publication_period_path(period_2)
+        }.to change(PublicationPeriod, :count).by(-1)
+      end
+    end
+    
+    context "publication period does not exist" do
+      it "returns not_found response" do
+        delete api_v1_publication_period_path(id: -1)
+        expect(response).to have_http_status(:not_found)
+      end
+      
+    end
+    
+  end
+  
+end
