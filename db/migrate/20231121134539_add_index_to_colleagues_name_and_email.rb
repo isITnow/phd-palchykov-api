270f8d3c675e061b440d0286e0e284f7d8c6bffd
Roman Serediuk <box4rom@gmail.com>
2023-11-21 16:25:53 +0200

    upd: add index to colleagues name; email

---
+++ db/migrate/20231121134539_add_index_to_colleagues_name_and_email.rb
@@ -0,0 +1,6 @@
+class AddIndexToColleaguesNameAndEmail < ActiveRecord::Migration[7.0]
+  def change
+    add_index :colleagues, :name, unique: true
+    add_index :colleagues, :email, unique: true
+  end
+end
---
+++ db/migrate/20231121141041_change_colleagues_phone_and_email_to_null.rb
@@ -0,0 +1,6 @@
+class ChangeColleaguesPhoneAndEmailToNull < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :colleagues, :phone, true
+    change_column_null :colleagues, :email, true
+  end
+end
