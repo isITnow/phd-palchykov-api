722fb47faf8432861082e92f06bb90e9684e280f
Roman Serediuk <box4rom@gmail.com>
2023-11-22 19:06:15 +0200

    refactor

--- spec/requests/api/v1/publication_periods_spec.rb
+++ spec/requests/api/v1/publication_periods_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "Api::V1::PublicationPeriods", type: :request do
+  let(:user) { create(:user) }
+  before { sign_in user }
+  let(:publication_period) { create(:publication_period) }
+
   describe "GET /api/v1/publication_periods" do
     it "returns a successful response" do
       get api_v1_publication_periods_path
@@ -8,7 +12,7 @@ describe "Api::V1::PublicationPeriods", type: :request do
     end
 
     it "returns publication periods" do
-      periods = create_list(:publication_period, 5)
+      create_list(:publication_period, 5)
       
       get api_v1_publication_periods_path
       expect(JSON.parse(response.body).length).to eq(5)  
@@ -16,59 +20,56 @@ describe "Api::V1::PublicationPeriods", type: :request do
   end
 
   describe "POST /api/v1/publication_periods" do
-    let(:user) { create(:user) }
-    before { sign_in user }
-    
+    context "with no user signed in" do
+      it "returns an unauthorized response" do
+        sign_out user
+        post api_v1_publication_periods_path params: { publication_period: { title: "2020-2023" } }
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
     context "with valid params" do
-      let(:valid_attributes) { {title: "2020-2023"} }
+      let(:params) do 
+        { publication_period: { title: "2020-2023" } } 
+      end
 
       it "returns a successful response" do
-        post api_v1_publication_periods_path params: { publication_period: valid_attributes }
+        post api_v1_publication_periods_path params: params
         expect(response).to have_http_status(:created)
       end
 
       it "creates a new PublicationPeriod" do
         expect {
-          post api_v1_publication_periods_path params: { publication_period: valid_attributes }
+          post api_v1_publication_periods_path params: params
         }.to change(PublicationPeriod, :count).by(1)
-        publication_period = PublicationPeriod.last
-        expect(publication_period.title).to eq("2020-2023")
+        @publication_period = PublicationPeriod.last
+        expect(@publication_period.title).to eq("2020-2023")
       end
     end
 
     context "with invalid params" do
-      let(:invalid_attributes) { { string: "2020-2023" } }
-      context "with no user signed in" do
-        it "returns an unauthorized response" do
-          sign_out user
-          post api_v1_publication_periods_path params: { publication_period: { title: "2020-2023" } }
-          expect(response).to have_http_status(:unauthorized)
-        end
+      let(:invalid_params) do
+        { publication_period: { title: nil }} 
       end
       
       it "returns failure response" do
-        post api_v1_publication_periods_path params: { publication_period: invalid_attributes }
+        post api_v1_publication_periods_path params: invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it "does not create a new PublicationPeriod" do
         expect {
-          post api_v1_publication_periods_path params: { publication_period: invalid_attributes }
+          post api_v1_publication_periods_path params: invalid_params
         }.to change(PublicationPeriod, :count).by(0)
       end
     end
   end
 
   describe "DELETE /api/v1/publication_periods/:id" do
-    let(:user) { create(:user) }
-    before { sign_in user }
-
-    let(:publication_period) {create(:publication_period)}
-    let(:publication_period_2) {create(:publication_period)}
-
     context "with no user signed in" do
       it "returns an unauthorized response" do
         sign_out user
+
         delete api_v1_publication_period_path(publication_period)
         expect(response).to have_http_status(:unauthorized)
       end
@@ -81,11 +82,11 @@ describe "Api::V1::PublicationPeriods", type: :request do
       end
       
       it "deletes a PublicationPeriod" do
-        publication_period
-        publication_period_2
+        create_list(:publication_period, 3)
+        @publication_period = PublicationPeriod.last
 
         expect {
-          delete api_v1_publication_period_path(publication_period_2)
+          delete api_v1_publication_period_path(@publication_period)
         }.to change(PublicationPeriod, :count).by(-1)
       end
     end
