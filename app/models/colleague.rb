01821d2cc87e419d6c98694114a54fce48a25594
Roman Serediuk <box4rom@gmail.com>
2024-01-08 10:08:06 +0200

    fix(email validation): allow nil

--- app/models/colleague.rb
+++ app/models/colleague.rb
@@ -1,8 +1,9 @@
 class Colleague < ApplicationRecord
   has_one_attached :photo
   
-  validates :name, :position, presence: true, length: { minimum: 5 }
+  validates :email, uniqueness: { allow_nil: true }, allow_nil: true
+  validates :name, presence: true, length: { minimum: 5 }, uniqueness: true
   validates :photo, presence: true
-  validates :name, :email, uniqueness: true
+  validates :position, presence: true, length: { minimum: 5 }
 end
 
