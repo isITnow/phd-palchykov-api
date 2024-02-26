ec174e169db16235da906cbb3db63332e616785a
Roman Serediuk <box4rom@gmail.com>
2024-02-26 12:04:51 +0200

    feat: add schema validation

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -1,4 +1,5 @@
 class Illustration < ApplicationRecord
+  include AttachedImageValidation
   include Validatable
 
   belongs_to :research
@@ -6,5 +7,10 @@ class Illustration < ApplicationRecord
   has_one_attached :schema
   
   validates :description, :schema, presence: true
+  validates :scema, presence: { message: 'must be attached' }, on: :create
+
+  validate do
+    validate_attached_image schema, 1
+  end
 end
 
