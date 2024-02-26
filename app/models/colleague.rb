dc1384f5fcda058b0d80e1b41d0ba3464fc8f9d5
Roman Serediuk <box4rom@gmail.com>
2024-02-26 11:49:43 +0200

    feat: photo validation

--- app/models/colleague.rb
+++ app/models/colleague.rb
@@ -1,9 +1,15 @@
 class Colleague < ApplicationRecord
+  include AttachedImageValidation
+
   has_one_attached :photo
   
   validates :email, uniqueness: { allow_nil: true }, allow_nil: true
   validates :name, presence: true, length: { minimum: 5 }, uniqueness: true
-  validates :photo, presence: true
+  validates :photo, presence: { message: 'must be attached' }, on: :create
   validates :position, presence: true, length: { minimum: 5 }
+
+  validate do
+    validate_attached_image photo, 1
+  end
 end
 
