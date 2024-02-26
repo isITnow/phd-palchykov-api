e89e286461a75615fcbf95dd6236f34be43f383f
Roman Serediuk <box4rom@gmail.com>
2024-02-26 12:01:06 +0200

    feat: add abstract and cover validation

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,10 +1,20 @@
 class Publication < ApplicationRecord
+  include AttachedImageValidation
   include Validatable
 
   belongs_to :publication_period
   
-  has_one_attached :cover
   has_one_attached :abstract
+  has_one_attached :cover
   
+  validates :abstract, :cover, presence: { message: 'must be attached' }, on: :create
   validates :title, :source, :source_url, :year, presence: true
+
+  validate do
+    validate_attached_image abstract, 1
+  end
+
+  validate do
+    validate_attached_image cover, 1
+  end
 end
